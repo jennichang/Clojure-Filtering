3f9473ca821aa51dc6cc09b53cc3e404a33b701d
Jennifer Chang <jennichang02@gmail.com>
2016-09-15 15:18:47 -0400

    fixed some things

--- clojure-hw/src/clojure_hw/core.clj
+++ clojure-hw/src/clojure_hw/core.clj
@@ -5,22 +5,22 @@
 
 (defn -main []
   (println "Type your category:")
-  (let [people (slurp "people.csv") ;reads the csv file into a string.
-        people (str/split-lines people) ;rebinding people to itself, but split into different lines
-        people (map (fn [line] ;rebind people, value is mapping. function baseing off of map is run split method
+  (let [purchase (slurp "purchases.csv") ;reads the csv file into a string.
+        purchase (str/split-lines purchase) ;rebinding people to itself, but split into different lines
+        purchase (map (fn [line] ;rebind people, value is mapping. function baseing off of map is run split method
                       (str/split line #",")) ;split current line on the comma. run map and pass people into map
-                    people)
-        header (first people) ;create new local variable "header", takes first item from people
-        people (rest people) ; redefines people to contain everything after first value
-        people (map (fn [line]
+                    purchase)
+        header (first purchase) ;create new local variable "header", takes first item from people
+        purchase (rest purchase) ; redefines people to contain everything after first value
+        purchase (map (fn [line]
                       (zipmap header line)) ;using strings in header as keys. will mix together line and header
-                    people)
+                      purchase)
         category-name (read-line) ;like scanner next line
-        people (filter (fn [line] ;filter takes in an argument.
+        purchase (filter (fn [line] ;filter takes in an argument.
         ; for each person, get the categor element from line map, see if that equals what was inputted from read-line
                          (= (get line "category") category-name))
-                       people)
+                         purchase)
         ;file-text (pr-str people)]
-        file-text (json/write-str people)] ;bind file-text parameter to people.converts map to json
+        file-text (json/write-str purchase)] ;bind file-text parameter to people.converts map to json
     ;(spit "filtered_people.edn" file-text)))
     (spit "filtered_purchases.json" file-text))) ;spits back into a file.
