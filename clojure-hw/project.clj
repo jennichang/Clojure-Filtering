514bd68fdc15255ec01c47d85dac036e390a6313
Jennifer Chang <jennichang02@gmail.com>
2016-09-15 14:35:25 -0400

    hw done

--- clojure-hw/project.clj
+++ clojure-hw/project.clj
@@ -1,6 +1,7 @@
 (defproject clojure-hw "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :main clojure_hw.core                                  ;need to define main method
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/data.json "0.2.6"]])
--- clojure-hw/src/clojure_hw/core.clj
+++ clojure-hw/src/clojure_hw/core.clj
@@ -1,6 +1,26 @@
-(ns clojure-hw.core)
+(ns clojure_hw.core
+  (:require [clojure.string :as str] [clojure.data.json :as json])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn -main []
+  (println "Type your category:")
+  (let [people (slurp "people.csv") ;reads the csv file into a string.
+        people (str/split-lines people) ;rebinding people to itself, but split into different lines
+        people (map (fn [line] ;rebind people, value is mapping. function baseing off of map is run split method
+                      (str/split line #",")) ;split current line on the comma. run map and pass people into map
+                    people)
+        header (first people) ;create new local variable "header", takes first item from people
+        people (rest people) ; redefines people to contain everything after first value
+        people (map (fn [line]
+                      (zipmap header line)) ;using strings in header as keys. will mix together line and header
+                    people)
+        category-name (read-line) ;like scanner next line
+        people (filter (fn [line] ;filter takes in an argument.
+        ; for each person, get the categor element from line map, see if that equals what was inputted from read-line
+                         (= (get line "category") category-name))
+                       people)
+        ;file-text (pr-str people)]
+        file-text (json/write-str people)] ;bind file-text parameter to people.converts map to json
+    ;(spit "filtered_people.edn" file-text)))
+    (spit "filtered_purchases.json" file-text))) ;spits back into a file.
